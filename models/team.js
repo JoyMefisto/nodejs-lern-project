5454a5089c175039601296b7be0d3bf8afc7e923
Artem Shanin <mefistoggc@mail.ru>
2018-02-07 21:12:39 +0300

    2-mongodb fixed Schema Team

--- models/team.js
+++ models/team.js
@@ -6,11 +6,10 @@ const Team = new Schema({
     name: String,
     game: String,
     url: String,
-    // players_id: [{ type: Schema.Types.ObjectId, ref: 'Player' }] // №1
-    //games: [{ type: Schema.Types.String, ref: 'Game' }] //todo: @codedojo хочу положить сюда все объекты из коллекции games
+}, {
+    versionKey: false // You should be aware of the outcome after set to false (add field '__v' )
 });
 
-// №2
 Team.virtual('players', {
     ref: 'Player', // The model to use
     localField: '_id', // Find people where `localField` относительно команды
@@ -19,14 +18,5 @@ Team.virtual('players', {
     // an array. `justOne` is false by default.
     justOne: false // если один то true
 });
-
-// Team.virtual('games', {
-//     ref: 'Game', // The model to use
-//     localField: 'game', // Find people where `localField` относительно команды
-//     foreignField: 'game', // is equal to `foreignField` относительно игрока
-//     // If `justOne` is true, 'members' will be a single doc as opposed to
-//     // an array. `justOne` is false by default.
-//     justOne: false // если один то true
-// });
 
 module.exports = mongoose.model('Team', Team);
